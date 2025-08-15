b98cf0eea05771ccbd6ac0c9d18249cf72a02636
beholdtheiceman <134699679+beholdtheiceman@users.noreply.github.com>
2025-08-14 20:55:44 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './App.jsx'
+import './index.css' // must import the CSS that contains @tailwind directives
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
