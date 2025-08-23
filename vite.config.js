4abc880140c7ba156fa13a6ec24383a18f09884a
beholdtheiceman <134699679+beholdtheiceman@users.noreply.github.com>
2025-08-23 00:00:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
+})
