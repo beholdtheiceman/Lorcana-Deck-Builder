870d692d758ca68596ff6b091110321fcf86b76b
Larry Meek <sportlarry@gmail.com>
2025-08-24 10:45:18 -0400

    Increase card image height in modal for better visibility

--- src/App.jsx
+++ src/App.jsx
@@ -4499,7 +4499,7 @@ function InspectCardModal({ open, card, onClose, onAdd }) {
           <img 
             src={imgSrc} 
             alt={card.name} 
-            className="w-auto h-auto max-w-[99.5%] max-h-[calc(80vh-200px)] object-contain rounded-xl border border-gray-800"
+            className="w-auto h-auto max-w-[99.5%] max-h-[calc(90vh-100px)] object-contain rounded-xl border border-gray-800"
           />
           
           {/* Close Button - positioned relative to the card image */}
